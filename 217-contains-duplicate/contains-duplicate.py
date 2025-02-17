b6b0cd04c63f7c7e7ebc5a236072476777919e0a
Jainesh-prog <74249503+Jainesh-prog@users.noreply.github.com>
2025-02-17 13:06:53 -0800

    Time: 8 ms (71.77%) | Memory: 31.6 MB (30.33%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,9 +1,7 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        checked = {}
-        for num in nums:
-            if num in checked and checked[num] >= 1:
-                return True
-            checked[num] = checked.get(num, 0) + 1
-        return False
+        if len(set(nums)) == len(nums):
+            return False
+        else:
+            return True
         
