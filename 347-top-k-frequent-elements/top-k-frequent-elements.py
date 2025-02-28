ad46d32e1b0e21b3028f257d2df926735584f22b
Jainesh-prog <74249503+Jainesh-prog@users.noreply.github.com>
2025-02-28 12:25:08 -0800

    Time: 4 ms (70.78%) | Memory: 21.1 MB (80.74%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,10 +1,11 @@
+from typing import List
+from collections import Counter
+import heapq
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count_map = {}
-        for num in nums:
-            count_map[num] = count_map.get(num,0) + 1
-        count_map = dict(sorted(count_map.items(), key=lambda item: item[1], reverse=True))
-        first_k_keys = list(count_map.keys())[:k]
-        return first_k_keys
-        
-        
+        # Step 1: Count frequencies
+        count_map = Counter(nums)  # Optimized counting
+
+        # Step 2: Use heap to get k most frequent elements
+        return [num for num, _ in heapq.nlargest(k, count_map.items(), key=lambda item: item[1])]
