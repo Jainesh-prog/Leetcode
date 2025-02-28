b3db57a01c2b6eb3350127d20dd1ba2a9e628a73
Jainesh-prog <74249503+Jainesh-prog@users.noreply.github.com>
2025-02-27 16:50:19 -0800

    Time: 47 ms (67.54%) | Memory: 20 MB (32.03%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -6,20 +6,13 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        if head is None or head.next is None:
-            return False
-        
-        # Traverse the list
-        while head is not None:
-            # Check if the value is already a float (indicating we've visited this node)
-            if isinstance(head.val, float):
-                return True  # Cycle detected
-
-            # Modify the value by adding 0.1 to mark it as visited
-            head.val += 0.1
-            head = head.next
-
-        return False 
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
 
 
             
