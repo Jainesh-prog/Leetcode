030b976104f7c3b3298223c65cfef226b7205404
Jainesh-prog <74249503+Jainesh-prog@users.noreply.github.com>
2025-04-19 11:45:56 -0700

    Time: 11 ms (23.02%) | Memory: 24.4 MB (7.72%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,18 +1,23 @@
 from typing import List
-from collections import Counter
-import heapq
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count = {}
-        freq = [[] for i in range(len(nums) + 1) ]
+        # Step 1: Count the frequency of each number using a hashmap
+        count = {}  # {num: frequency}
         for n in nums:
-            count[n] = 1 + count.get(n,0)
-        for n,c in count.items():
+            count[n] = 1 + count.get(n, 0)
+
+        # Step 2: Create buckets where index = frequency
+        # Each bucket[i] will contain all numbers that appear i times
+        freq = [[] for i in range(len(nums) + 1)]  # max possible freq is len(nums)
+        for n, c in count.items():
             freq[c].append(n)
+
+        # Step 3: Traverse buckets in reverse (from highest freq to lowest)
         res = []
-        for i in range(len(freq) - 1, 0 , -1):
+        for i in range(len(freq) - 1, 0, -1):
             for n in freq[i]:
                 res.append(n)
+                # Stop when we’ve collected top k frequent elements
                 if len(res) == k:
                     return res
